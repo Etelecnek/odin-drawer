67155d977cbbbfc646e50d529d0a7842f10bfcb2
Etelecnek <126229297+Etelecnek@users.noreply.github.com>
2023-04-03 22:14:18 -0700

    Cells change color on mousedown

--- javascript.js
+++ javascript.js
@@ -1,13 +1,19 @@
 const container = document.querySelector('.container');
 
+
 function makeGrid(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement('div');
-        cell.innerText = (c + 1);
+        let cell = document.createElement('button');
+        cell.addEventListener('mousedown', function() {
+            this.style.backgroundColor = "red";
+        });
         container.appendChild(cell).className = "grid-item";
     };
 };
+
+//Create Function on Mouse Down enable Hover Button Change Colors
+//Create Function on Mouse Up disable Hover Button to change colors
 
 makeGrid(16, 16);
