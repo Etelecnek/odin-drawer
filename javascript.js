10fb4627055a8d94c475377e20e38a51c30d5340
Etelecnek <126229297+Etelecnek@users.noreply.github.com>
2023-04-05 22:16:47 -0700

    Cells change color holding mousedown and moving and stop changing color on mouseup

--- javascript.js
+++ javascript.js
@@ -6,14 +6,19 @@ function makeGrid(rows, cols) {
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement('button');
-        cell.addEventListener('mousedown', function() {
-            this.style.backgroundColor = "red";
+        function color() {
+            cell.style.backgroundColor = "red";
+        }
+        addEventListener('mousedown', function() {
+            cell.addEventListener('mousedown', color);
+            cell.addEventListener('mouseover', color);
+            
+        });
+        addEventListener('mouseup', function() {
+            cell.removeEventListener('mouseover', color);
         });
         container.appendChild(cell).className = "grid-item";
     };
 };
-
-//Create Function on Mouse Down enable Hover Button Change Colors
-//Create Function on Mouse Up disable Hover Button to change colors
 
 makeGrid(16, 16);
