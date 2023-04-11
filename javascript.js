574251ac4f6afbb8167c7804cf0eee7f59f942ff
Etelecnek <126229297+Etelecnek@users.noreply.github.com>
2023-04-10 19:02:18 -0700

    Added colors, added width and hight adjustment

--- javascript.js
+++ javascript.js
@@ -1,24 +1,56 @@
+let color;
 const container = document.querySelector('.container');
-
+const Red = document.querySelector('.Red');
+const Blue = document.querySelector('.Blue');
+const Green = document.querySelector('.Green');
+const Black = document.querySelector('.Black');
+const White = document.querySelector('.White');
+const Entersize = document.querySelector('.Entersize');
 
 function makeGrid(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement('button');
-        function color() {
-            cell.style.backgroundColor = "red";
+        function colorin() {
+            cell.style.backgroundColor = color;
         }
         addEventListener('mousedown', function() {
-            cell.addEventListener('mousedown', color);
-            cell.addEventListener('mouseover', color);
+            cell.addEventListener('mousedown', colorin);
+            cell.addEventListener('mouseover', colorin);
             
         });
         addEventListener('mouseup', function() {
-            cell.removeEventListener('mouseover', color);
+            cell.removeEventListener('mouseover', colorin);
         });
         container.appendChild(cell).className = "grid-item";
     };
 };
 
-makeGrid(16, 16);
+function colorinRed() {
+    color = "#FF0000";
+}
+function colorinBlue() {
+    color = "#0000FF";
+}
+function colorinGreen() {
+    color = "#00FF00";
+}
+function colorinBlack() {
+    color = "#000000";
+}
+function colorinWhite() {
+    color = "#FFFFFF";
+}
+function EnterWidthHeight () {
+    var W = document.getElementById('Width').value;
+    var H = document.getElementById('Height').value;
+    makeGrid(W, H);
+}
+
+Red.addEventListener('click', colorinRed);
+Blue.addEventListener('click', colorinBlue);
+Green.addEventListener('click', colorinGreen);
+Black.addEventListener('click', colorinBlack);
+White.addEventListener('click', colorinWhite);
+Entersize.addEventListener('click', EnterWidthHeight);
